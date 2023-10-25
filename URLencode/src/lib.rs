dc70ec717b8adc69fda9e039bd1606446215df89
plscallMeAlex <alexkung007@gmail.com>
2023-10-26 01:02:54 +0700

    second commit

--- URLencode/src/lib.rs
+++ URLencode/src/lib.rs
@@ -1,7 +1,8 @@
+use clap::error::Result;
 use clap::{Args, Parser, Subcommand, builder};
 use std::error::Error;
 use std::{io, clone};
-use std::io::{Read,Write};
+use std::io::{Read,Write, BufReader};
 use std::str;
 use std::string::FromUtf8Error;
 use std::fs;
@@ -93,18 +94,17 @@ impl Form2 {
                 if self.flg1 {
                     decoding_percent(&inp)
                 } else if self.flg2 {
-                    return "".to_string(); //wiil change when to base64 is finish
+                    frombase64(&inp).unwrap() //wiil change when to base64 is finish
                 } else {
                     decoding_percent(&inp) //for default choice
                 }
             },
             StringOrPath::Path(path) => {
-
                 let filecontent = readfile(&path.clone().into_os_string().into_string().unwrap());
                 if self.flg1 {
                     decoding_percent(&filecontent.unwrap())
                 } else if self.flg2 {
-                    return "".to_string(); //wiil change when to base64 is finish
+                    frombase64(&filecontent.unwrap()).unwrap() //wiil change when to base64 is finish
                 } else {
                     decoding_percent(&filecontent.unwrap()) //for default choice
                 }
@@ -225,6 +225,17 @@ fn matchingb64(inp:u8) -> u8{
     }
 }
 
+fn matchingb642(inp:u8) -> Option<u8> {
+    match inp{
+        b'A'..=b'Z' => Some(inp - b'A'),
+        b'a'..=b'z' => Some(inp - b'a' + 26),
+        b'0'..=b'9' => Some(inp - b'0' + 52),
+        b'+' => Some(62),
+        b'/' => Some(63),
+        _ => None,
+    }
+}
+
 trait ChecklastDig {
     fn fromlastindex(&self, n:usize) -> char;
 }
@@ -310,18 +321,83 @@ pub fn binoperate3(inp: &[u8]) -> Result<Vec<u8>, Box<dyn Error>>{
 }
 
 // //ANCHOR - Decoding function from base64
-pub fn frombase64(inp:&str) -> String {
-    if inp.fromlastindex(0) == '='{
-        if inp.fromlastindex(1) == '='{
-            println!("have 2");
-        } else {
-            println!("have 1")
-        }
-    } else {
-        println!("Not have");
+//NOTE - iter from the first char then if it found = then it will stop
+
+// pub fn frombase64(input: &str) -> Result<String, Box<dyn Error>> {
+//     // Remove padding characters
+//     let input = input.trim_end_matches('=');
+
+//     // Split input into 6-bit chunks
+//     let chunks: Vec<&str> = input.chars().collect::<Vec<char>>().chunks(6).map(|c| c.iter().collect()).collect();
+
+//     // Convert 6-bit chunks to 8-bit bytes
+//     let bytes: Vec<u8> = chunks.into_iter()
+//         .map(|chunk| {
+//             let num = u8::from_str_radix(chunk, 2)?;
+//             Ok(num << 2) // Shift left by 2 bits (to convert 6 bits to 8 bits)
+//         })
+//         .collect::Result<Vec<u8>,Box<Error>>?;
+
+//     // Decode percent encoding
+//     let decoded_bytes = decoding_percent(&String::from_utf8(bytes)?);
+
+//     Ok(decoded_bytes)
+// }
+
+fn frombase64(inp: &str) -> Option<String> {
+    let rmpad = inp.trim_end_matches('=');
+    let numpad = inp.chars().filter(|&c| c == '=').count();
+    let mut buffer = Vec::new();
+    for i in rmpad.as_bytes() {
+        let rev1 = matchingb642(*i).unwrap();
+        buffer.push(rev1);
     }
-    todo!()
+
+    let mut decoded_bytes = Vec::new();
+    for i in 0..buffer.len() {
+        match i % 4 {
+            0 => decoded_bytes.push((buffer[i] << 2) | (buffer[i + 1] >> 4)),
+            1 => decoded_bytes.push(((buffer[i] & 0b1111) << 4) | (buffer[i + 1] >> 2)),
+            2 => decoded_bytes.push(((buffer[i] & 0b11) << 6) | buffer[i + 1]),
+            _ => (),
+        }
+    }
+    String::from_utf8(decoded_bytes).ok()
 }
+//     for &byte in input.as_bytes() {
+//         if byte == b'=' {
+//             break; // Padding character, stop decoding
+//         }
+        
+//         if let Some(&value) = char_to_value.get(byte as usize) {
+//             if value != -1 {
+//                 buffer = (buffer << 6) | (value as u32);
+//                 buffer_size += 6;
+
+//                 while buffer_size >= 8 {
+//                     buffer_size -= 8;
+//                     result.push(((buffer >> buffer_size) & 0xFF) as u8);
+//                 }
+//             } else {
+//                 return None; // Invalid Base64 character
+//             }
+//         } else {
+//             return None; // Invalid input character
+//         }
+//     }
+
+//     // Check for leftover bits in the buffer
+//     if buffer_size >= 8 {
+//         return None; // Invalid Base64 encoding
+//     }
+
+//     // Convert the decoded bytes to a UTF-8 string
+//     match String::from_utf8(result) {
+//         Ok(decoded_string) => Some(decoded_string),
+//         Err(_) => None, // Invalid UTF-8 sequence
+//     }
+// }
+
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Unit test
@@ -344,6 +420,7 @@ fn testtobase64(){
     // let res2 = tobase64("d");
     // println!("{}",res2);
     let a = frombase64(&res);
+    // println!("{}",a.unwrap());
 }
 
 #[test]
